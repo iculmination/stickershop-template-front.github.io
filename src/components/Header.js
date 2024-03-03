8bf1e367ea5edd43ccaca26f45b1b4dec95a5f87
culmination <ovsiichuk.bohdan@gmail.com>
2024-03-03 04:14:19 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <div className=" bg-gray-200 w-full h-24 flex justify-between items-center">
+      <nav className="container">
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
